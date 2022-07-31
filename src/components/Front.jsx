431b9d03e4d61c569b3a826dffb4ba60d39ec9c4
Dan Gleason <dannycgleason@gmail.com>
2022-07-31 14:49:00 -0400

    changed h1

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -159,7 +159,7 @@ const userNavigation = [
 //   { name: 'Create account', href: '#' },
 ]
 const product = {
-  name: "Bundle. Gift meaningfully.",
+  name: "Give a Bundle. Gift meaningfully.",
   href: '#',
   price: '$45',
   description:
