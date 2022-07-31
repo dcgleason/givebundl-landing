5e3b7455fd4e4a34875c937bf2a03b1e0b0e52c7
Dan Gleason <dannycgleason@gmail.com>
2022-07-31 14:14:00 -0400

    changed wording

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -807,7 +807,7 @@ const options = {
                   </div>
 
                   <div className="mt-4 space-y-6">
-                    <p className="text-base text-gray-500"><b>Bundle</b>,  <em>noun</em>: a book of written messages of love and support collected for a special person in your life from <em>his or her</em> family and friends.<br></br><br></br> We coordinate all of the logistics for you, deliver within a two week window, and give <em>you</em> all the credit.</p>
+                    <p className="text-base text-gray-500"><b>Bundle</b>,  <em>noun</em>: a book of written messages of love and support collected for a special person in your life from <em>his or her</em> family and friends.<br></br><br></br> We coordinate all of the logistics for you and will deliver a physical book of written messages to your recipient within a two week window.</p>
                   </div>
                   <div className="mt-6 text-center">
                     <a href="#" className="group inline-flex text-base font-medium">
