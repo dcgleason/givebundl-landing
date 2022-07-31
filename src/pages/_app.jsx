5db0c31559f91d0447637cdd04293fd9d53cfa24
Dan Gleason <dannycgleason@gmail.com>
2022-07-31 13:17:04 -0400

    added google analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,31 @@
 import 'focus-visible'
 import '@/styles/tailwind.css'
+import Script from 'next/script'
+import { NextScript } from 'next/document'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  return (
+
+  <>
+
+  <Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=G-Q7EKYQCWR0`}> </Script>
+  <Script strategy='lazyOnload'>{
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag(){dataLayer.push(arguments);}
+  
+  gtag('js', new Date());
+  gtag('config', 'G-Q7EKYQCWR0');
+
+}</Script>
+
+  <Head>
+                <title>Bundle</title>
+                <meta name="viewport" content="initial-scale=1, width=device-width" />
+   </Head>
+  <Component {...pageProps} />
+  </>
+  )
+  
 }
