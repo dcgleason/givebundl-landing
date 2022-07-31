1fd4821b474f8cb55484c2b5693089b07aaceba7
Dan Gleason <dannycgleason@gmail.com>
2022-07-31 16:17:41 -0400

    changed sitemap slightly

--- src/pages/sitemap.xml.jsx
+++ src/pages/sitemap.xml.jsx
@@ -11,18 +11,15 @@ export const getServerSideProps = async ({ res }) => {
     const sitemap = `
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
 
-    <script/>
 
     <url>
     <loc>https://bundle.love/</loc>
-    <lastmod>2022-7-31</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.9</priority>
     </url>
 
     <url>
     <loc>https://bundle.love/messages</loc>
-    <lastmod>2022-7-31</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.9</priority>
     </url>
