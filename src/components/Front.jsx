9ead0364d7fa8f73a215d90de495f59d80fbae7b
Dan Gleason <dannycgleason@gmail.com>
2022-07-31 14:23:15 -0400

    changed link to /messages

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -932,7 +932,7 @@ const options = {
                       height='500' />
                   </div>
                   <p className="mt-8 text-base text-gray-500">
-                    1. Family, friends, and/or coworkers of your Bundle recipient write heartfelt messages from prompts on our <a className="underline font-bold" href="https://bundle.love/write">Write</a> page about your Bundle recipient (we coordinate this for you).All Bundle contributors have five days to submit their responses.
+                    1. Family, friends, and/or coworkers of your Bundle recipient write heartfelt messages from prompts on our <a className="underline font-bold" href="https://bundle.love/messages">Write</a> page about your Bundle recipient (we coordinate this for you).All Bundle contributors have five days to submit their responses.
                   </p>
                 </div>
                 <div>
