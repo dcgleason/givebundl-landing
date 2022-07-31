82c38d2ff3f9ba67738a52db309e23e00151312e
Dan Gleason <dannycgleason@gmail.com>
2022-07-31 16:15:28 -0400

    changed sitemap

--- src/pages/sitemap.xml.jsx
+++ src/pages/sitemap.xml.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const SiteMap = (props) => {
 
-  const sitemap = `
-  
+const Sitemap = () => {
+    return null;
+};
+
+export const getServerSideProps = async ({ res }) => {
+    const BASE_URL = 'http://bundle.love';
+
+    const sitemap = `
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
 
     <script/>
@@ -23,18 +28,15 @@ const SiteMap = (props) => {
     </url>
 
     </urlset>
-    `
+  `;
 
     res.setHeader('Content-Type', 'text/xml');
     res.write(sitemap);
     res.end();
 
-return{
+    return {
+        props: {},
+    };
+};
 
-  props: {}
-
-
-}
-}
-
-export default SiteMap;
+export default Sitemap;
