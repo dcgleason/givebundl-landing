d87d6f62362c37498ac0f55029e6e50e8963e158
Dan Gleason <dannycgleason@gmail.com>
2022-07-31 09:04:27 -0400

    espcape quote issues

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -596,7 +596,7 @@ const options = {
 				      	Try the beta version of <em>Bundle</em> 
                 </Dialog.Title>
                 <Dialog.Description className="text-xl m-2">
-                  (it is free! We'll send you an email with instructions.)
+                  (it is free! We&aposll send you an email with instructions.)
                 </Dialog.Description>
                 <form className="mt-2 flex-col sm:max-w-md" >
                 <label className="text-gray-700 underline text-sm font-bold">
@@ -676,7 +676,7 @@ const options = {
                       className="w-full bg-[#f8ad9d] hover:bg-[#f4978e] border border-transparent rounded-md shadow-sm py-2 px-4 flex items-center justify-center text-base font-medium text-white  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#f08080]"
                       onClick={() => setIsModalOpen(true)}
                    >
-                      Try Beta Version (it's free)
+                      Try Beta Version (it&aposs free)
                     </button>
 
                 <div className="hidden lg:ml-8 lg:flex">
@@ -796,7 +796,7 @@ const options = {
                         className="flex-shrink-0 mr-2 h-6 w-6 text-gray-400 group-hover:text-gray-500"
                         aria-hidden="true"
                       />
-                      <span className="text-gray-500 hover:text-gray-700">We have a money-back guarantee, but we guarentee you won't need it 😉</span>
+                      <span className="text-gray-500 hover:text-gray-700">We have a money-back guarantee, but we guarentee you won&apost need it 😉</span>
                     </a>
                   </div>
 
@@ -896,7 +896,7 @@ const options = {
           <section aria-labelledby="details-heading">
             <div className="flex flex-col items-center text-center">
               <h2 id="details-heading" className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                Here's how your Bundle is put together:
+                Here&aposs how your Bundle is put together:
               </h2>
               <p className="mt-3 max-w-3xl text-lg text-gray-600">
                 (This all happens within a two week window)
@@ -938,7 +938,7 @@ const options = {
                When to send a Bundle:
               </h2>
               <p className="mt-3 max-w-3xl text-lg text-gray-600">
-               (Below are some of our favorite excuses to "share the love.")
+               (Below are some of our favorite excuses to &quotshare the love.&quot)
               </p>
             </div>
             <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 lg:gap-x-8">
@@ -949,7 +949,7 @@ const options = {
                   <path strokeLinecap="round" strokeLinejoin="round" d="M21 15.546c-.523 0-1.046.151-1.5.454a2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.701 2.701 0 00-1.5-.454M9 6v2m3-2v2m3-2v2M9 3h.01M12 3h.01M15 3h.01M21 21v-7a2 2 0 00-2-2H5a2 2 0 00-2 2v7h18zm-3-9v-2a2 2 0 00-2-2H8a2 2 0 00-2 2v2h12z" />
                 </svg>
                   <h3 className="mt-6 text-base font-medium text-gray-900">Birthdays</h3>
-                  <p className="mt-3 text-base text-gray-500">Wish someone a "happy birthday."</p>
+                  <p className="mt-3 text-base text-gray-500">Wish someone a happy birthday.</p>
                 </div>
                 <div>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-24 w-auto text-[#f8ad9d]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
@@ -979,7 +979,7 @@ const options = {
                   <path strokeLinecap="round" strokeLinejoin="round" d="M3 19v-8.93a2 2 0 01.89-1.664l7-4.666a2 2 0 012.22 0l7 4.666A2 2 0 0121 10.07V19M3 19a2 2 0 002 2h14a2 2 0 002-2M3 19l6.75-4.5M21 19l-6.75-4.5M3 10l6.75 4.5M21 10l-6.75 4.5m0 0l-1.14.76a2 2 0 01-2.22 0l-1.14-.76" />
                   </svg>
                   <h3 className="mt-6 text-base font-medium text-gray-900">A Thank You</h3>
-                  <p className="mt-3 text-base text-gray-500">Send a "group thank you" to a special someone.</p>
+                  <p className="mt-3 text-base text-gray-500">Send a group thank you to a special someone.</p>
                 </div>
                 <div>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-24 w-auto text-[#f8ad9d]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
@@ -1000,7 +1000,7 @@ const options = {
                       <path strokeLinecap="round" strokeLinejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                 </svg>
                   <h3 className="mt-6 text-base font-medium text-gray-900">Just Because</h3>
-                  <p className="mt-3 text-base text-gray-500">Let someone know how much they're loved, just because they are.</p>
+                  <p className="mt-3 text-base text-gray-500">Let someone know how much they&aposre loved, just because they are.</p>
                 </div>
               {/* ))} */}
             </div>
