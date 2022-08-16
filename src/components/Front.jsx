aac6c1b10c752753227b7f2932f9f3d1ea13b2df
Dan Gleason <dannycgleason@gmail.com>
2022-08-16 16:57:13 -0400

    test

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -987,10 +987,10 @@ const options = {
                     <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
                   </svg>
-                  <h3 className="mt-6 text-base font-medium text-gray-900">Work Anniversaries</h3>
-                  <p className="mt-3 text-base text-gray-500">A great way to recognize an employee and his or her work anniversary. <br/> <br/>  Acknowledge the essential role they play for your team and thank them for all their contributions in the workplace. Happier staff equals higher employee productivity!</p>
+                  <h3 className="mt-6 text-base font-medium text-gray-900">Birthdays</h3>
+                  <p className="mt-3 text-base text-gray-500"> <br/> <br/>  </p>
                 </div> 
-                <div>
+                <div>s
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-24 w-auto text-[#f8ad9d]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                   </svg>
