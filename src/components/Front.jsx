e4866427d7a83004cbc625cec3efdad3859ccb49
Dan Gleason <dannycgleason@gmail.com>
2022-08-16 17:08:59 -0400

    removed errors

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -979,7 +979,7 @@ const options = {
                   <path strokeLinecap="round" strokeLinejoin="round" d="M21 15.546c-.523 0-1.046.151-1.5.454a2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.701 2.701 0 00-1.5-.454M9 6v2m3-2v2m3-2v2M9 3h.01M12 3h.01M15 3h.01M21 21v-7a2 2 0 00-2-2H5a2 2 0 00-2 2v7h18zm-3-9v-2a2 2 0 00-2-2H8a2 2 0 00-2 2v2h12z" />
                 </svg>
                   <h3 className="mt-6 text-base font-medium text-gray-900">Birthdays</h3>
-                  <p className="mt-3 text-base text-gray-500">Wish someone a "happy birthday."</p>
+                  <p className="mt-3 text-base text-gray-500">Wish someone a happy birthday.</p>
                 </div>
                 <div>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-24 w-auto text-[#f8ad9d]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
@@ -1009,7 +1009,7 @@ const options = {
                   <path strokeLinecap="round" strokeLinejoin="round" d="M3 19v-8.93a2 2 0 01.89-1.664l7-4.666a2 2 0 012.22 0l7 4.666A2 2 0 0121 10.07V19M3 19a2 2 0 002 2h14a2 2 0 002-2M3 19l6.75-4.5M21 19l-6.75-4.5M3 10l6.75 4.5M21 10l-6.75 4.5m0 0l-1.14.76a2 2 0 01-2.22 0l-1.14-.76" />
                   </svg>
                   <h3 className="mt-6 text-base font-medium text-gray-900">A Thank You</h3>
-                  <p className="mt-3 text-base text-gray-500">Send a "group thank you" to a special someone.</p>
+                  <p className="mt-3 text-base text-gray-500">Send a group thank you to a special someone.</p>
                 </div>
                 <div>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-24 w-auto text-[#f8ad9d]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
@@ -1030,7 +1030,7 @@ const options = {
                       <path strokeLinecap="round" strokeLinejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                 </svg>
                   <h3 className="mt-6 text-base font-medium text-gray-900">Just Because</h3>
-                  <p className="mt-3 text-base text-gray-500">Let someone know how much they're loved, just because they are.</p>
+                  <p className="mt-3 text-base text-gray-500">Let someone know how much they are loved, just because they are.</p>
                 </div>
               {/* ))} */}
             </div>
