74f9c7417cf457f151f3caf544cf6dd45c2948f7
Dan Gleason <dannycgleason@gmail.com>
2022-08-18 09:55:25 -0400

    update wording

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -824,7 +824,7 @@ const options = {
                       <ShieldCheckIcon
                         className="flex-shrink-0 mr-2 h-6 w-6 text-gray-400 group-hover:text-gray-500"
                         aria-hidden="true" />
-                      <span className="text-gray-500 hover:text-gray-700">Guaranteed to make you feel like a great friend, sister, brother, etc.</span>
+                      <span className="text-gray-500 hover:text-gray-700">Gaurenteed to promote feelings of love and connectedness</span>
                     </a>
                   </div>
 
