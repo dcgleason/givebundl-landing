669f24d4879986ae2f74822bc6d032c82aed37a6
Dan Gleason <dannycgleason@gmail.com>
2022-08-31 13:45:16 -0500

    added address label

--- src/pages/messages.jsx
+++ src/pages/messages.jsx
@@ -105,7 +105,7 @@ const Messages = () => {
           <div>
             <h3 className="text-lg mt-20 leading-6 font-medium text-gray-900">Write</h3>
             <p className="mt-1 max-w-2xl text-sm text-gray-500">
-              Please answer the questions below about the gift recipient.
+              Please answer the questions below your friend / family member / etc.
             </p>
           </div>
 
@@ -129,7 +129,7 @@ const Messages = () => {
             </div>
             <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
               <label htmlFor="username" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                Recipients address (provided to you via email):
+                Recipient name and address (Name, Street, City, State, Zip, Country):
               </label>
               <div className="mt-1 sm:mt-0 sm:col-span-2">
                 <div className="max-w-lg flex rounded-md shadow-sm">
