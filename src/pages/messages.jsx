ecb2504c15c67d0471e0de93a6a0ca41c93f506b
Dan Gleason <dannycgleason@gmail.com>
2022-09-01 13:49:53 -0500

    italics

--- src/pages/messages.jsx
+++ src/pages/messages.jsx
@@ -270,7 +270,7 @@ const Messages = () => {
             
             <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
               <label htmlFor="username" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-              <b>Elle Young</b> just had a baby - her second. To show your support, take this opportunity to tell her how much she means to you. How has she impacted you in positive ways? How and why are you grateful for her presence in your life?
+              <em>Elle Young</em> just had a baby - her second. To show your support, take this opportunity to tell her how much she means to you. How has she impacted you in positive ways? How and why are you grateful for her presence in your life?
               </label>
               <div className="mt-1 sm:mt-0 sm:col-span-2">
                 <div className="max-w-lg flex rounded-md shadow-sm">
