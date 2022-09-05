e9a0e873013630355d8e1ae762c57d1cdf796807
Dan Gleason <dannycgleason@gmail.com>
2022-09-05 09:29:19 -0500

    upload

--- src/pages/messages.jsx
+++ src/pages/messages.jsx
@@ -331,8 +331,10 @@ const Messages = () => {
                   <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" style={{ strokeWidth: 2, strokeLinecap:"round", strokeLinejoin:"round" }}/>
                 </svg>
                 <div className="flex text-sm text-gray-600">
+                {   selectedImage ?  <label htmlFor="file-upload"> <span className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"> Image Ready to Send</span> </label>:  (<div>
                   <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                   {selectedImage ? <span> Image Ready to Send</span> : <span>Upload a image</span>}
+                  
+                    <span>Upload a image</span> 
                     <input
                         id="file-upload" 
                        name="file-upload" 
@@ -344,6 +346,8 @@ const Messages = () => {
                       className="sr-only"/>
                   </label>
                   <p className="pl-1">here</p>
+                  </div> 
+                  )}
                 </div>
                 <p className="text-xs text-gray-500">PNG or JPG up to 16MB</p>
               </div>
