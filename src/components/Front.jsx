d2cba4149f4d4a7cf63779e991131b3cfd501a14
Dan Gleason <dannycgleason@gmail.com>
2022-10-02 10:30:32 -0400

    update removed font

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -782,7 +782,7 @@ const options = {
                 </div>
 
                 <div className="mt-4">
-                  <h1 className="text-3xl font-extrabold font-apple tracking-tight text-gray-900 sm:text-4xl">{product.name}</h1>
+                  <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">{product.name}</h1>
                 </div>
 
                 <section aria-labelledby="information-heading" className="mt-4">
