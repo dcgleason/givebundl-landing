eb383850024cfebecd8ce92310918fca7dd7dba2
Dan Gleason <dannycgleason@gmail.com>
2022-10-03 14:29:57 -0400

    removed menu item

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -154,7 +154,7 @@ const navigation = {
   pages: [
     { name: 'Home', href: '#' },
   //  { name: "Write", href: 'http://usebundle.co/messages'},
-   { name: "Add Contributors", href: 'http://usebundle.co/contributors'} // need a way for the browser to known what gift the person is writing for / adding contributors to (and they can only add contributors once) - git code;
+  //  { name: "Add Contributors", href: 'http://usebundle.co/contributors'} // need a way for the browser to known what gift the person is writing for / adding contributors to (and they can only add contributors once) - git code;
 
   ],
 }
