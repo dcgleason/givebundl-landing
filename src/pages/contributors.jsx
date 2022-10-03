ded3ad646b0cb54da54c8a216e746ebe1c2548ab
Dan Gleason <dannycgleason@gmail.com>
2022-10-03 09:05:08 -0400

    update contrib page

--- src/pages/contributors.jsx
+++ src/pages/contributors.jsx
@@ -84,9 +84,9 @@ const options = {
           <rect width={404} height={404} fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
         </svg>
         <div className="text-center">
-          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl"> Pre-Order a Bundle book</h2>
+          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl"> Add Contributors to Your Bundle</h2>
           <p className="mt-4 text-lg leading-6 text-gray-500">
-            We will collect all the necessary details from you (contributor email addresses, shipping information, etc.) in a subsequent email!
+           We will email them to remind them, and include your message in the email!
           </p>
         </div>
         <div className="mt-12">
