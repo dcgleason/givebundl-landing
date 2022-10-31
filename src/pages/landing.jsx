cfe2c057b2e4e348e46fc4551280dd9fb2c6bb94
Dan Gleason <dannycgleason@gmail.com>
2022-10-30 22:20:18 -0400

    update function

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,10 +1,9 @@
 
 import React, { useState, useEffect } from 'react';
-import  Link  from 'next/link'
-import Image from 'next/image'
 import { AILandingPage } from '@/components/index'
 
-export default function Landing() {
+const Landing = () => {
+
     return (
       <>
        <AILandingPage/>
@@ -12,3 +11,4 @@ export default function Landing() {
     )
   }
   
+  export default Landing;
