8c25fa12c70e015920e11a0a553a5c183d0e43c6
Dan Gleason <dannycgleason@gmail.com>
2022-10-31 16:01:50 -0400

    update

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -961,7 +961,7 @@ const options = {
               <div className="flex flex-col items-center pb-10 text-center">
               <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
       <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-        It&apos;s time to show your loved one just how <em>much</em> they&apos;re loved.
+        It&apos;s time to show your loved one just how much they&apos;re loved.
       </h2>{" "}
       <p className="text-base text-gray-700 md:text-lg">
         Give a Bundle book on the following occasions.
