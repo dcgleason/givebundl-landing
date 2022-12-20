b3824d6b03486099c23aaf6289950de2a59dda3f
Dan Gleason <dannycgleason@gmail.com>
2022-12-20 11:55:15 -0500

    fixed error

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -158,7 +158,7 @@ const userNavigation = [
 //   { name: 'Create account', href: '#' },
 ]
 const product = {
-  name: "Love not just an emotion, it's an action.",
+  name: "Love is not just an emotion, it's an action.",
   href: '#',
   price: '$99',
   description:
