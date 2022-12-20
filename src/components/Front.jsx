9bfffa8addcd5f53d42ed1d0659fba7ad1355e1f
Dan Gleason <dannycgleason@gmail.com>
2022-12-20 11:26:57 -0500

    update

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -158,7 +158,7 @@ const userNavigation = [
 //   { name: 'Create account', href: '#' },
 ]
 const product = {
-  name: "Love isn't a feeling, it's an action.",
+  name: "The isn't just a emotion, it's an action.",
   href: '#',
   price: '$99',
   description:
