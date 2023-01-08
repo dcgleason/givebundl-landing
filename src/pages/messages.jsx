ee2575b3a06bf1271d4511c2d9e7771a2c52b2b3
Dan Gleason <dannycgleason@gmail.com>
2023-01-08 18:22:52 -0500

    changed so that we can upload image to s3 and display or store it

--- src/pages/messages.jsx
+++ src/pages/messages.jsx
@@ -26,107 +26,37 @@ const Messages = () => {
   const [selectedImage, setSelectedImage] = useState(null);
   const [ success, setSuccess ] = useState(false);
   const [ failure, setFailure ] = useState(false);
-  const [imageUrl, setImageUrl] = useState(null);
-
-  useEffect(() => {
-    if (selectedImage) {
-      setImageUrl(URL.createObjectURL(selectedImage));
-      console.log('selectedimage ' + selectedImage);
-    }
-  }, [selectedImage]);
-
-  const sendNoteCard = async ()=> {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Authorization", "Bearer "+ process.env.THANKS_TOKEN);
-
-    var raw = JSON.stringify({
-      "image_template": 49934,
-      "handwriting_style": 12,
-      "message": questionOne,
-      "recipients": [
-        {
-          "name": name,
-          "address": street,
-          "city": city,
-          "province": state,
-          "postal_code": zip,
-          "country": "USA"
-        }
-      ]
-    });
-
-      var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-      };
-      
-      fetch("https://api.thanks.io/api/v2/send/notecard", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
-    
-  }
-
-  const postMessagesMongoDB = async (e) => {
-    e.preventDefault();
-    sendNoteCard();
-    try{
-    //   // image upload
-
-    //   if(picture){
-
-    //   const formData = new FormData();
-    //   formData.append("file", picture.pictureAsFile);
+  const [file, setFile] = useState(null);
+  const [caption, setCaption] = useState("");
   
-    //   console.log(picture.pictureAsFile);
+    const submit = async (event) => {
+      event.preventDefault();
   
-    //   for (var key of formData.entries()) {
-    //     console.log(key[0] + ", " + key[1]);
-    //   }
+      const formData = new FormData();
+      formData.append("image", file);
+      formData.append("caption", caption);
   
-    //   const data = await fetch("https://yay-api.herokuapp.com/gift/imageupload", {
-    //     method: "post",
-    //     headers: { "Content-Type": "multipart/form-data" },
-    //     body: formData,
-    //   });
-    //   const uploadedImage = await data.json();
-    //   if (uploadedImage) {
-    //     console.log("Successfully uploaded image");
-    //   } else {
-    //     console.log("Error Found");
-    //   }
-    // }
-      // message upload
-      const resp =  await fetch("https://yay-api.herokuapp.com/gifts/messages", { 
-        method: 'POST', 
-        headers: { 
-          'Content-type': 'application/json'
-         }, 
-        body: JSON.stringify({
-          createdAt: Date.now(),
-          contributorName: contributorName, 
-          recipientName: name,
-          questionOne: questionOne,
-          recipientStreet: street,
-          recipientCity: city,
-          recipientState: state,
-          recipientZip: zip,
-          recipientCountry: 'USA',
-          giftCode: giftCode, // need to pass in the gift code from somewhere...when the person ordres it,
-          image: selectedImage
-        }) 
-        });
-        setSuccess(true);
-     
-    }
-    catch (error){
-      console.log("error error"+ error);
-      setFailure(true);
-    }
-  }
+      const { url } = await fetch("http://localhost:3001/s3Url").then((res) =>
+        res.json()
+      );
+  
+      console.log(url);
+  
+      //post the image to the s3 bucket
+      await fetch(url, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        body: file,
+      });
+  
+      const imageUrl = url.split("?")[0];
+      console.log(imageUrl); //this is the url of the image in the s3 bucket -- you can use this to display the image (or store it in the database)
+    };
+  
+
+ 
 
   return (
     <>
@@ -138,7 +68,7 @@ const Messages = () => {
     {success? <Success/> : <div></div>}
     {failure? <Failure/> : <div></div>}
       <form className="space-y-8 divide-y divide-gray-200 lg:px-32 lg:mx-32 shadow-md rounded border-gray-200 border"
-        onSubmit={postMessagesMongoDB}
+        onSubmit={submit}
         >
       <div className="space-y-8 divide-y divide-gray-200 sm:space-y-5">
         <div>
@@ -185,120 +115,7 @@ const Messages = () => {
                 </div>
               </div>
             </div>
-            <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-          
-              
-              <div className="sm:col-span-2">
-                <label htmlFor="address"  className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-">
-                  Street address
-                </label>
-                <div className="mt-1">
-                  <input
-                    type="text"
-                    name="address"
-                    onChange={e => setStreet(e.target.value)}
-                    value={street}
-                    id="address"
-                    placeholder="123 Main Street"
-                    autoComplete="street-address"
-                    className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"
-                    required
-                  />
-                </div>
-              </div>
 
-              <div className="sm:col-span-2">
-                <label htmlFor="apartment"  className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-">
-                 Apartment, suite, etc.
-                </label>
-                <div className="mt-1">
-                  <input
-                    type="text"
-                    onChange={e => setApartment(e.target.value)}
-                    value={apartment}
-                    name="apartment"
-                    id="apartment"
-                    className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"
-                />
-                </div>
-              </div>
-              <div>
-                <label htmlFor="city"  className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-">
-                  City
-                </label>
-                <div className="mt-1">
-                  <input
-                    type="text"
-                    name="city"
-                    id="city"
-                    onChange={e => setCity(e.target.value)}
-                    value={city}
-                    autoComplete="address-level2"
-                    placeholder="New Albany"
-                    className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"
-                    required
-                  />
-                </div>
-              </div>
-
-              <div>
-                <label htmlFor="country"  className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-">
-                  Country
-                </label>
-                <div className="mt-1">
-                  <select
-                    id="country"
-                    name="country"
-                    onChange={e => setCountry(e.target.value)}
-                    value={country}
-                    className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"
-                    required
-                >
-                    <option>United States</option>
-                    <option>Canada</option>
-                    <option>Mexico</option>
-                  </select>
-                </div>
-              </div>
-
-              <div>
-                <label htmlFor="region"  className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-">
-                 State / Province
-                </label>
-                <div className="mt-1">
-                  <input
-                    type="text"
-                    name="region"
-                    id="region"
-                    placeholder="OH"
-                    onChange={e => setState(e.target.value)}
-                    value={state}
-                    autoComplete="address-level1"
-                    className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"
-                    required
-                  />
-                </div>
-              </div>
-
-              <div>
-                <label htmlFor="postal-code"  className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-">
-                  Postal code
-                </label>
-                <div className="mt-1">
-                  <input
-                    type="text"
-                    name="postal-code"
-                    onChange={e => setZip(e.target.value)}
-                    value={zip}
-                    id="postal-code"
-                    placeholder='43054'
-                    autoComplete="postal-code"
-                    className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"
-                    required
-                />
-                </div>
-              </div>
-            </div>
             
             <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
               <label htmlFor="username" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
@@ -323,30 +140,21 @@ const Messages = () => {
     
 
 
-            <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
           <label htmlFor="cover-photo" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Photo: </label>
           <div className="mt-1 sm:mt-0 sm:col-span-2">
             <div className="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
               <div className="space-y-1 text-center">
-                { !selectedImage? <svg className="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                { file !== null ? <svg className="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
                   <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" style={{ strokeWidth: 2, strokeLinecap:"round", strokeLinejoin:"round" }}/>
                 </svg> : <div> </div>}
                 <div className="flex text-sm text-gray-600">
-                {   selectedImage ?  <label htmlFor="file-upload"> <span className="relative bg-white rounded-md font-medium text-grey-700 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"> Image Ready to Send</span> </label>:  (<>
+                {   file !== null?  <label htmlFor="file-upload"> <span className="relative bg-white rounded-md font-medium text-grey-700 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"> Image Ready to Send</span> </label>:  (<>
                   <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                  
-                    <span>Upload a image</span> 
-                    <input
-                        id="file-upload" 
-                       name="file-upload" 
-                       type="file" 
-                       onChange={(event) => { 
-                          console.log(event.target.files[0]);
-                          setSelectedImage(event.target.files[0]);
-                          }} 
-                      className="sr-only"/>
+    
+                    <input name='file-upload' onChange={(e) => setFile(e.target.files[0])} type='file' accept='image/*'/>
                   </label>
-                  <p className="pl-1">here</p>
+
                   </>
                   )}
                 </div>
