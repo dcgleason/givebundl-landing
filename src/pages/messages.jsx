aebc3656b9484ef02d28b97db1c42d21882b6990
Dan Gleason <dannycgleason@gmail.com>
2023-01-12 17:20:18 -0500

    update to image uploading section

--- src/pages/messages.jsx
+++ src/pages/messages.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useSearchParams } from "react-router-dom";
 import { useEffect } from 'react'
 import Box from '@material-ui/core/Box';
 import Image from 'next/image';
@@ -145,20 +146,21 @@ const Messages = () => {
           <div className="mt-1 sm:mt-0 sm:col-span-2">
             <div className="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
               <div className="space-y-1 text-center">
-                { file !== null ? <svg className="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                { file == null ? <svg className="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
                   <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" style={{ strokeWidth: 2, strokeLinecap:"round", strokeLinejoin:"round" }}/>
                 </svg> : <div> </div>}
                 <div className="flex text-sm text-gray-600">
-                {   file !== null?  <label htmlFor="file-upload"> <span className="relative bg-white rounded-md font-medium text-grey-700 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"> Image Ready to Send</span> </label>:  (<>
+                {   file !== null?  <label htmlFor="file-upload"> <span className="relative bg-white rounded-md font-medium text-grey-700 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"> Image Ready to Send.</span> </label>:  (<>
                   <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
     
                     <input name='file-upload' onChange={(e) => setFile(e.target.files[0])} type='file' accept='image/*'/>
+                    <p className="text-xs text-gray-500">PNG or JPG up to 16MB</p>
                   </label>
 
                   </>
                   )}
                 </div>
-                <p className="text-xs text-gray-500">PNG or JPG up to 16MB</p>
+                
               </div>
             </div>
           </div>
