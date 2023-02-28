4a90031586d9ec7d54e882eb1bd07e637abd1ad2
Dan Gleason <dannycgleason@gmail.com>
2023-02-28 14:04:42 -0500

    update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -22,7 +22,7 @@ const HubspotContactForm = () => {
 
     return (
         <div>
-            <h1 className="text-2xl mb-12 font-bold tracking-tight text-gray-900 sm:text-4xl" > In today's fast paced world, people often struggle to find the time to express their feelings to their family and friends. Bundl help your family and friends solve that problem. Sign up here to be the first to hear when we launch!</h1>
+            <h1 className="text-2xl mb-12 font-bold tracking-tight text-gray-900 sm:text-4xl" > In today&apos;s fast paced world, people often struggle to find the time to express their feelings to their family and friends. Bundl help your family and friends solve that problem. Sign up here to be the first to hear when we launch!</h1>
             <div id="hubspotForm"></div>
         </div>
     );
