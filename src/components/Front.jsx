14111ad93cf5862b1c6503dd28e3f5d5d6063b6a
Dan Gleason <dannycgleason@gmail.com>
2023-03-02 13:41:06 -0500

    added italics

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -961,7 +961,7 @@ const options = {
               <div className="flex flex-col items-center pb-10 text-center">
               <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
       <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-        Boost employee retention by instilling a felt sense of belonging into individuals.
+        Boost employee retention by instilling <em>a felt sense</em> of belonging into individuals.
       </h2>{" "}
       <p className="text-base text-gray-700 md:text-lg">
         Give a Bundl book with our app on the following occasions:
