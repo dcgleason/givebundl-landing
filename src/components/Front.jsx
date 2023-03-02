290931029cf129c398c51da9b7f589e4b7897d32
Dan Gleason <dannycgleason@gmail.com>
2023-03-02 13:33:57 -0500

    update to page wording

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -964,7 +964,7 @@ const options = {
         Show your teammate or direct-report that they belong.
       </h2>{" "}
       <p className="text-base text-gray-700 md:text-lg">
-        Give a Bundl book with our app on the following milestones:
+        Give a Bundl book with our app on the following occasions:
       </p>
     </div>
                 {/* <h2 id="details-heading" className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
