93adeec66d1825d320f71e91cc930d42f37a2638
Dan Gleason <dannycgleason@gmail.com>
2023-03-03 13:17:35 -0500

    update for slack app copy

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -22,7 +22,7 @@ const HubspotContactForm = () => {
 
     return (
         <div>
-            <h1 className="text-2xl mb-12 font-bold tracking-tight text-gray-900 sm:text-4xl" >Sign up to learn more.</h1>
+            <h1 className="text-2xl mb-12 font-bold tracking-tight text-gray-900 sm:text-4xl" >Sign up to be a beta-tester.</h1>
             <div id="hubspotForm"></div>
         </div>
     );
