46f1e6b673579446051d221f4bfeee91f78f66da
Dan Gleason <dannycgleason@gmail.com>
2023-03-05 11:47:09 -0500

    update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -22,7 +22,7 @@ const HubspotContactForm = () => {
 
     return (
         <div>
-            <h1 className="text-2xl mb-12 font-bold tracking-tight text-gray-900 sm:text-4xl" >Retain your top talent. Sign up to learn more.</h1>
+            <h1 className="text-2xl mb-12 font-bold tracking-tight text-gray-900 sm:text-4xl" >Availalbe (soon) on Slack. Sign up to learn to hear when we launch.</h1>
             <div id="hubspotForm"></div>
         </div>
     );
